58a055f86cb9e909034e189e71fc78880c240f24
叶子 <xiaoyexiang@126.com>
2017-08-26 12:00:48 +0800

    第一节，stylus和字体图标设置

---
+++ src/components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    yezi
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Yezi'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+  @import "common/stylus/mixin.styl"
+
+</style>
