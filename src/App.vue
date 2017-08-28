139e31159944be8c83c922b1ea4a034ddbaaf102
叶子 <xiaoyexiang@126.com>
2017-08-28 09:28:43 +0800

    第3章，头部和tab组件开发完成
    m-header, tab

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
+    <m-header></m-header>
+    <tab></tab>
     <router-view></router-view>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app'
-}
+<script type="text/ecmascript-6">
+  import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
+
+  export default {
+    components: {
+      MHeader,
+      Tab
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
