1273e5c0195d2ee1252c04fca9397c7829a427d1
叶子 <xiaoyexiang@126.com>
2017-09-08 09:31:29 +0800

    获取歌手列表数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,33 @@
 <template>
-    <div>
-  singer
-    </div>
+  <div>
+    singer
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {}
+  import {getSingerList} from 'api/singer.js'
+  import {ERR_OK} from 'api/config'
+
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created: function () {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+            console.log(this.singers)
+          }
+        })
+      }
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
