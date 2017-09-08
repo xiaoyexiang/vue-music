6f7014b6f438494a046f697dcabfc42a12437591
叶子 <xiaoyexiang@126.com>
2017-09-09 00:21:29 +0800

    处理歌手列表数据
    抽取热门、从a-z排序

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,9 +5,12 @@
 </template>
 
 <script type="text/ecmascript-6">
-  import {getSingerList} from 'api/singer.js'
+  import {getSingerList} from 'api/singer'
+  import Singer from 'common/js/singer'
   import {ERR_OK} from 'api/config'
 
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
   export default {
     data() {
       return {
@@ -22,9 +25,53 @@
         getSingerList().then((res) => {
           if (res.code === ERR_OK) {
             this.singers = res.data.list
-            console.log(this.singers)
+            console.log(this._normalizeSinger(this.singers))
           }
         })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          // 获取前面作为热门数据
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+          // a-z创建
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+        // 为了有序列表，我们需要处理 map
+        let ret = []
+        let hot = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
       }
     }
   }
