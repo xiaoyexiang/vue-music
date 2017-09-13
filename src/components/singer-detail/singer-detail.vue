723dd9e2b87f984f841c770a62b86537129cb78a
叶子 <xiaoyexiang@126.com>
2017-09-14 00:19:29 +0800

    歌手数据抓取
    使用jsonp抓取qq音乐接口的数据

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -8,6 +8,9 @@
 
 <script type="text/ecmascript-6">
   import {mapGetters} from 'vuex'
+  import {getSingerDetail} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
 
   export default {
     computed: {
@@ -15,8 +18,37 @@
         'singer'
       ])
     },
+    data() {
+      return {
+        songs: []
+      }
+    },
     created: function () {
       console.log(this.singer)
+      this._getSingerDetail()
+    },
+    methods: {
+      _getSingerDetail() {
+        if (!this.singer.id) {
+          this.$router.push('/singer')
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(this._normallizeSongs(res.data.list))
+          }
+        })
+      },
+      _normallizeSongs(list) {
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} = item
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
     }
   }
 </script>
