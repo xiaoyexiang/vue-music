ed855c7489391a83f91f5bde5b770badd1c9a9b3
叶子 <xiaoyexiang@126.com>
2017-09-13 19:56:10 +0800

    vuex初始化
    singer -> 数据配置

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -10,6 +10,7 @@
   import {getSingerList} from 'api/singer'
   import Singer from 'common/js/singer'
   import {ERR_OK} from 'api/config'
+  import {mapMutations} from 'vuex'
 
   const HOT_NAME = '热门'
   const HOT_SINGER_LEN = 10
@@ -27,6 +28,7 @@
         this.$router.push({
           path: `/singer/${singer.id}`
         })
+        this.setSinger(singer)
       },
       _getSingerList() {
         getSingerList().then((res) => {
@@ -79,7 +81,10 @@
           return a.title.charCodeAt(0) - b.title.charCodeAt(0)
         })
         return hot.concat(ret)
-      }
+      },
+      ...mapMutations({
+        setSinger: 'SET_SINGER'
+      })
     },
     components: {
       Listview
