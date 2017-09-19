b535909d909866b8c994f3ae22877da23c705bf5
叶子 <xiaoyexiang@126.com>
2017-09-20 00:21:32 +0800

    music-list组件剩余开发
    web开发APP操作交互 - 难点
    要复习它实现原理

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="play-wrapper">
-        <div ref="playBtn" class="play">
+        <div ref="playBtn" v-show="songs.length>0" class="play" v-cloak>
           <i class="icon-play"></i>
           <span class="text">随机播放全部</span>
         </div>
@@ -14,9 +14,18 @@
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
-    <scroll :listen-scroll="listenScroll" :probe-type="probeType" :data="songs" class="list" ref="list">
+    <scroll @scroll="scroll"
+            :listen-scroll="listenScroll"
+            :probe-type="probeType"
+            :data="songs"
+            class="list"
+            ref="list"
+    >
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
+      </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
       </div>
     </scroll>
   </div>
@@ -26,6 +35,11 @@
   import Scroll from 'base/scroll/scroll'
   import Loading from 'base/loading/loading'
   import SongList from 'base/song-list/song-list'
+  import {prefixStyle} from 'common/js/dom'
+
+  const RESERVED_HEIGHT = 40
+  const transform = prefixStyle('transform')
+  const backdrop = prefixStyle('backdrop-filter')
 
   export default {
     props: {
@@ -42,20 +56,62 @@
         default: ''
       }
     },
+    data() {
+      return {
+        scrollY: 0
+      }
+    },
     computed: {
       bgStyle() {
         return `background-image: url(${this.bgImage})`
       }
     },
-    created(){
+    created() {
       this.listenScroll = true
       this.probeType = 3
     },
     mounted() {
       this.imageHeight = this.$refs.bgImage.clientHeight
+      this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
       this.$refs.list.$el.style.top = `${this.imageHeight}px`
     },
-    methods: {},
+    methods: {
+      scroll(pos) {
+        this.scrollY = pos.y
+      },
+      back() {
+        this.$router.back()
+      }
+    },
+    watch: {
+      scrollY(newVal) {
+        let translateY = Math.max(this.minTranslateY, newVal)
+        let zIndex = 0
+        let scale = 1
+        let blur = 0
+        const percent = Math.abs(newVal / this.imageHeight)
+        if (newVal > 0) {
+          scale = 1 + percent
+          zIndex = 10
+        } else {
+          blur = Math.min(20, percent * 20)
+        }
+        if (newVal < this.minTranslateY) {
+          zIndex = 10
+          this.$refs.bgImage.style.paddingTop = 0
+          this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none'
+        } else {
+          this.$refs.bgImage.style.paddingTop = '70%'
+          this.$refs.bgImage.style.height = 0
+          this.$refs.playBtn.style.display = 'block'
+        }
+        this.$refs.bgImage.style.zIndex = zIndex
+        this.$refs.bgImage.style[transform] = `scale(${scale})`
+        this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
+        this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+      }
+    },
     components: {
       Scroll,
       Loading,
