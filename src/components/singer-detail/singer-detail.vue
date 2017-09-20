df84170766844d1924d559b104d2d4b17aa4db92
叶子 <xiaoyexiang@126.com>
2017-09-21 00:33:50 +0800

    player组件创建、vuex相关数据设计
    1、player组件创建
    2、vuex相关数据设计
    3、控制显示player（全局，在app.vue）

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -70,14 +70,4 @@
 
   .slide-enter, .slide-leave-to
     transform: translate3d(100%, 0, 0)
-
-  .singer-detail {
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
-  }
 </style>
