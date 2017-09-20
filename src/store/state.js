df62b415b86c9c935cca487423531a3b2c5739f2
叶子 <xiaoyexiang@126.com>
2017-09-21 00:37:59 +0800

    vuex state.js 注释

--- src/store/state.js
+++ src/store/state.js
@@ -2,12 +2,12 @@ import {playMode} from 'common/js/config'
 
 const state = {
   singer: {},
-  playing: false,
-  fullScreen: false,
-  playlist: [],
-  sequenceList: [],
-  mode: playMode.sequence,
-  currentIndex: -1
+  playing: false,              // 播放状态
+  fullScreen: false,           // 全屏播放
+  playlist: [],                // 播放列表
+  sequenceList: [],            // 顺序列表
+  mode: playMode.sequence,     // 播放模式 0，1，2
+  currentIndex: -1             // index
 }
 
 export default state
