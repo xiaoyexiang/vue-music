8c2178d3cb4b358155c085ca0baa2fa377d1a8df
叶子 <xiaoyexiang@126.com>
2017-09-30 00:16:08 +0800

    创建排行榜top-list.vue组件
    1、top-list.vue基础结构
    2、vuex相关数据管理

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -2,7 +2,7 @@
   <div class="rank" ref="rank">
     <Scroll :data="topList" class="toplist" ref="toplist">
       <ul>
-        <li class="item" v-for="item in topList">
+        <li class="item" @click="selectItem(item)" v-for="item in topList">
           <div class="icon">
             <img width="100" height="100" v-lazy="item.picUrl"/>
           </div>
@@ -18,6 +18,7 @@
         <loading></loading>
       </div>
     </Scroll>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -27,6 +28,7 @@
   import Scroll from 'base/scroll/scroll'
   import Loading from 'base/loading/loading'
   import {playlistMixin} from 'common/js/mixin'
+  import {mapMutations} from 'vuex'
 
   export default {
     mixins: [playlistMixin],
@@ -44,14 +46,23 @@
         this.$refs.rank.style.bottom = bottom
         this.$refs.toplist.refresh()
       },
+      selectItem(item) {
+        this.$router.push({
+          path: `/rank/${item.id}`
+        })
+        console.log(item)
+        this.setTopList(item)
+      },
       _getTopList() {
         getTopList().then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.topList)
             this.topList = res.data.topList
           }
         })
-      }
+      },
+      ...mapMutations({
+        setTopList: 'SET_TOP_LIST'
+      })
     },
     components: {
       Scroll,
--- src/store/state.js
+++ src/store/state.js
@@ -8,7 +8,8 @@ const state = {
   sequenceList: [],            // 顺序列表
   mode: playMode.sequence,     // 播放模式 0，1，2
   currentIndex: -1,            // 播放index
-  disc: {}                     // 歌单
+  disc: {},                    // 歌单
+  topList: {}                 // 排行歌单列表
 }
 
 export default state
