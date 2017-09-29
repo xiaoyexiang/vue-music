f498e229388e94e79ab2f863844e9c3fb50f79c7
叶子 <xiaoyexiang@126.com>
2017-09-29 11:24:13 +0800

    歌单详情组件disc完成
    数据请求需要服务端请求

--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -1,11 +1,14 @@
 <template>
   <transition name="slide">
-    <music-list :title="title" :bg-image="bgImage"></music-list>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
   import MusicList from 'components/music-list/music-list'
+  import {getSongList} from 'api/recommend'
+  import {createSong} from 'common/js/song'
+  import {ERR_OK} from 'api/config'
   import {mapGetters} from 'vuex'
 
   export default {
@@ -23,6 +26,32 @@
     data() {
       return {
         songs: []
+      }
+    },
+    created() {
+      this._getSongList()
+    },
+    methods: {
+      _getSongList() {
+        console.log(this.disc)
+        if (!this.disc.dissid) {
+          this.$router.push('/recommend')
+          return
+        }
+        getSongList(this.disc.dissid).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.cdlist[0].songlist)
+          }
+        })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((musicData) => {
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
       }
     },
     components: {
