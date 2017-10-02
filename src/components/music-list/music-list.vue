8d6f71c0ca499b52e84909adaf38bda17730208a
叶子 <xiaoyexiang@126.com>
2017-10-02 23:01:51 +0800

    排行榜歌曲列表组件扩展
    注意song-list music-list 之间的修改

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -22,7 +22,7 @@
             ref="list"
     >
       <div class="song-list-wrapper">
-        <song-list @select="selectItem" :songs="songs"></song-list>
+        <song-list :rank="rank" @select="selectItem" :songs="songs"></song-list>
       </div>
       <div class="loading-container" v-show="!songs.length">
         <loading></loading>
@@ -57,6 +57,10 @@
       title: {
         type: String,
         default: ''
+      },
+      rank: {
+        type: Boolean,
+        default: false
       }
     },
     data() {
--- src/components/top-list/top-list.vue
+++ src/components/top-list/top-list.vue
@@ -1,12 +1,15 @@
 <template>
   <transition name="slide">
-    <music-list :title="title" :bg-image="bgImage"></music-list>
+    <music-list :rank="rank" :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
   import MusicList from 'components/music-list/music-list'
   import {mapGetters} from 'vuex'
+  import {getMusicList} from 'api/rank'
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
 
   export default {
     computed: {
@@ -19,6 +22,37 @@
       ...mapGetters([
         'topList'
       ])
+    },
+    data() {
+      return {
+        songs: [],
+        rank: true
+      }
+    },
+    created() {
+      this._getMusicList()
+    },
+    methods: {
+      _getMusicList() {
+        if (!this.topList.id) {
+          this.$router.push('/rank')
+        }
+        getMusicList(this.topList.id).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normallizeSongs(res.songlist)
+          }
+        })
+      },
+      _normallizeSongs(list) {
+        let ret = []
+        list.forEach((item) => {
+          const musicData = item.data
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
     },
     components: {
       MusicList
