9b5dbe5ec0fedecc9846da4a529e1f6ec4504827
叶子 <xiaoyexiang@126.com>
2017-10-09 15:02:24 +0800

    抓取热门搜索关键词

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -3,13 +3,44 @@
     <div class="search-box-wrapper">
       <search-box ref="searchBox"></search-box>
     </div>
+    <div class="shortcut-wrapper">
+      <div class="shortcut">
+        <div class="hot-key">
+          <h2 class="title">热门搜索</h2>
+          <ul>
+            <li class="item" v-for="item in hotKey">
+              <span v-text="item.k"></span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import SearchBox from 'base/search-box/search-box'
+  import {getHotKey} from 'api/search'
+  import {ERR_OK} from 'api/config'
 
   export default {
+    created() {
+      this._getHotKey()
+    },
+    data() {
+      return {
+        hotKey: []
+      }
+    },
+    methods: {
+      _getHotKey() {
+        getHotKey().then((res) => {
+          if (res.code === ERR_OK) {
+            this.hotKey = res.data.hotkey.slice(0, 10)
+          }
+        })
+      }
+    },
     components: {
       SearchBox
     }
